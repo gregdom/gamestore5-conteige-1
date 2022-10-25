f932010c1c6e659be26e0d5ef61f74f2a153fe3c
Gregory Domingos <58437322+gregdom@users.noreply.github.com>
2022-10-25 19:33:35 -0300

    Delete src directory

--- src/modules/users/routes/notification.routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import NotificationController from "../controllers/NotificationController.js";
-
-const notificationRouter = Router();
-const notificationControllerInit = new NotificationController();
-
-notificationRouter.post("/notification", notificationControllerInit.create);
