41822b2e3181c78ad20ac6a71600f515b753deb7
Gregory Domingos <58437322+gregdom@users.noreply.github.com>
2022-10-26 09:11:18 -0300

    Update notification.routes.js

--- src/modules/users/routes/notification.routes.js
+++ src/modules/users/routes/notification.routes.js
@@ -4,6 +4,6 @@ import NotificationController from "../controllers/NotificationController.js";
 const notificationRouter = Router();
 const notificationControllerInit = new NotificationController();
 
-notificationRouter.post("/notification", notificationControllerInit.create);
+notificationRouter.post("/", notificationControllerInit.create);
 
 export default notificationRouter;
